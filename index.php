0310479a8df94262a80ad94ee178ed3d97f4a05d
MariePorquier <marieporquier@free.fr>
2019-12-03 14:55:07 +0100

    phppartie3exo1

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHPpartie3exo1et2</title>
+  </head>
+  <body>
+    <?php
+    echo $months[5];
+    ?>
+  </body>
+</html>
